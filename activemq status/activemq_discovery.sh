642fa97a710546f461519e8c09951cf4e81aea95
lubin <lubin@belle.com.cn>
2018-06-27 15:36:21 +0800

    activemq status

--- activemq status/activemq_discovery.sh
+++ activemq status/activemq_discovery.sh
@@ -3,11 +3,11 @@ discovery() {
 MQ_IP=(10.240.10.35 10.240.10.36)
             for g in ${MQ_IP[@]}
             do 
-            curl -uadmin:PaaSword http://${g}:4401/admin/queues.jsp >/dev/null 2>&1
+            curl -uadmin:admin http://${g}:4401/admin/queues.jsp >/dev/null 2>&1
             if [ $? -eq 0 ]; then
             i=$g
             fi
-            port=($(curl -uadmin:PaaSword http://${i}:4401/admin/queues.jsp 2>/dev/null| grep -A 5 "${QUEUENAME}</a></td>"|awk -F '<' '{print $0}'|sed 's/<\/td>//g'|sed 's/<.*>//g;/^$/d;/--/d'|grep '^[a-Z]'))
+            port=($(curl -uadmin:admin http://${i}:4401/admin/queues.jsp 2>/dev/null| grep -A 5 "${QUEUENAME}</a></td>"|awk -F '<' '{print $0}'|sed 's/<\/td>//g'|sed 's/<.*>//g;/^$/d;/--/d'|grep '^[a-Z]'))
             done 
             printf '{\n'
             printf '\t"data":[\n'
